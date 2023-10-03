fe8c49952763fb4d0512f88b7b61c1cfba8e6395
“MuhammadNurMaulana” <muhammadnrmaulana@gmail.com>
2023-10-03 19:06:31 +0700

    update market

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -6,12 +6,14 @@ import { Container } from "../components/Container/Container";
 import { SwiperSlide } from "swiper/react";
 import { SwiperJs } from "../swiper/Swiper";
 import beasts from "../service/beast.json";
+import gbeast from "../service/goldbeast.json";
+import lbeast from "../service/lightbeas.json";
 export const Market = () => {
   return (
     <div className="bg-[#2B2B2B] text-white box-border">
       <Navbar />
       <Container>
-        <h1>Baby Beast NFTs</h1>
+        <h1 className="font-workSans font-semibold text-2xl">Baby Beast NFTs</h1>
         <SwiperJs>
           {babys.map((baby, index) => (
             <SwiperSlide key={index} className="bg-[#3B3B3B] rounded-xl overflow-hidden ml-8">
@@ -27,9 +29,41 @@ export const Market = () => {
             </SwiperSlide>
           ))}
         </SwiperJs>
-        <h1>Ancient Beast NFTs</h1>
+        <h1 className="font-workSans font-semibold text-2xl">Ancient Beast NFTs</h1>
         <SwiperJs>
           {beasts.map((beast, index) => (
+            <SwiperSlide key={index} className="bg-[#3B3B3B] rounded-xl overflow-hidden ml-8">
+              <img src={beast.src} />
+              <div className="py-3 px-5 flex flex-col gap-4">
+                <h1 className="font-workSans font-semibold text-xl">{beast.title}</h1>
+                <div className="flex justify-between items-center">
+                  <p>Price</p>
+                  <p className="font-spaceMono">{beast.price}</p>
+                </div>
+                <button className="border border-secondary py-2 px-4 rounded-md hover:bg-secondary transition-all duration-200 hover:duration-300">Buy Now</button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </SwiperJs>
+        <h1 className="font-workSans font-semibold text-2xl">Light Mythical NFTs</h1>
+        <SwiperJs>
+          {lbeast.map((beast, index) => (
+            <SwiperSlide key={index} className="bg-[#3B3B3B] rounded-xl overflow-hidden ml-8">
+              <img src={beast.src} />
+              <div className="py-3 px-5 flex flex-col gap-4">
+                <h1 className="font-workSans font-semibold text-xl">{beast.title}</h1>
+                <div className="flex justify-between items-center">
+                  <p>Price</p>
+                  <p className="font-spaceMono">{beast.price}</p>
+                </div>
+                <button className="border border-secondary py-2 px-4 rounded-md hover:bg-secondary transition-all duration-200 hover:duration-300">Buy Now</button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </SwiperJs>
+        <h1 className="font-workSans font-semibold text-2xl">Golden King NFTs</h1>
+        <SwiperJs>
+          {gbeast.map((beast, index) => (
             <SwiperSlide key={index} className="bg-[#3B3B3B] rounded-xl overflow-hidden ml-8">
               <img src={beast.src} />
               <div className="py-3 px-5 flex flex-col gap-4">
